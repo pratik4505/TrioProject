39443eb1a375a6bcb93643eadbe8e0dc0f3c0d59
harshrajatul <harshrajatul01@gmail.com>
2023-11-19 14:14:10 +0530

    added chatlist part

---
+++ websterFrontend/src/React Main/Components/Message/ChatList.jsx
@@ -0,0 +1,28 @@
+import React, {useState,useEffect,useContext}from 'react'
+const baseUrl = "http://localhost:3000";
+import GlobalContext from '../../context/GlobalContext';
+import "./chatList.scss";
+export default function ChatList(props) {
+
+    const [unreadMsg,setUnreadMsg]=useState(undefined);
+    const gloContext = useContext(GlobalContext);
+    
+    gloContext.socket.on("receiveMessage",(data)=>{
+     
+        if(props.currChat&&props.currChat.chatId!==props.chat.chatId&&data.senderId===props.chat.otherMemberId){
+            setUnreadMsg(data.message);
+        }
+    })
+  
+  return (
+    <div onClick={()=>{props.onChatClick(props.chat)}} >
+        {props.chat.otherMemberImageUrl&&<img
+          src={`${baseUrl}/${props.chat.otherMemberImageUrl}`}
+          alt="Profile"
+          className="profile-image"
+        />}
+        <p>{props.chat.otherMemberName}</p>
+        {unreadMsg&&props.currChat.chatId!==props.chat.chatId&&<p className='unread'>{unreadMsg}</p>}
+      </div>
+  )
+}
