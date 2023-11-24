e17271b5a67c46ba573adacc69422faf786c805d
ayushkumar135 <ayushhkumar135@gmail.com>
2023-11-25 00:24:56 +0530

    navbar made responsive
    navbar made responsive

--- websterFrontend/src/React Main/Components/Navbar.jsx
+++ websterFrontend/src/React Main/Components/Navbar.jsx
@@ -6,72 +6,76 @@ import { BsBriefcase } from "react-icons/bs";
 import { BiUserCircle } from "react-icons/bi";
 import { BiSolidMessageDetail } from "react-icons/bi";
 import { IoIosNotifications } from "react-icons/io";
+import { BsBriefcaseFill } from "react-icons/bs";
 import { MdEmojiPeople } from "react-icons/md";
 import { Cookies } from 'react-cookie';
 
 export default function Navbar() {
   const cookies = new Cookies();
   const ownerId=cookies.get('userId');
+  const [showMenu, setShowMenu] = React.useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
   return (
     <div className="navbar-container">
-      
-      
-      <div className="nav-bar">
-      <img src="/Lyncwoback.png"></img>
-      
-        <div className="link-bar">
-          <Link to="/">
+      <div className={`nav-bar ${showMenu ? 'show-menu' : ''}`}>
+        <div className="img-ham">
+      <img src="/Lyncwoback.png" alt="" />
+        <div className="menu-icon" onClick={toggleMenu}>
+          <div className={`bar ${showMenu ? 'open' : ''}`}></div>
+          <div className={`bar ${showMenu ? 'open' : ''}`}></div>
+          <div className={`bar ${showMenu ? 'open' : ''}`}></div>
+        </div>
+        </div>
+        <div className={`link-bar ${showMenu ? 'show-menu' : ''}`}>
+        <Link to="/">
             <AiFillHome size={40} className="react-icons" />
           </Link>
           <span>
-            <Link to="/">Home</Link>
+          <Link to="/">Home</Link>
           </span>
         </div>
-        
-        <div className="link-bar">
-          <Link to="/Jobs">
-            <BsBriefcase size={40} className="react-icons" />
+        <div className={`link-bar ${showMenu ? 'show-menu' : ''}`}>
+        <Link to="/Jobs">
+            < BsBriefcaseFill size={40} className="react-icons" />
           </Link>
           <span>
-            <Link to="/Jobs">Jobs</Link>
+          <Link to="/Jobs">Jobs</Link>
           </span>
         </div>
-        
-        <div className="link-bar">
-          <Link to="/Requests">
+        <div className={`link-bar ${showMenu ? 'show-menu' : ''}`}>
+        <Link to="/Requests">
             <MdEmojiPeople size={40} className="react-icons" />
           </Link>
           <span>
-            <Link>Request</Link>
+          <Link to="/Requests">Request</Link>
           </span>
         </div>
-        
-        <div className="link-bar">
-          <Link to="/Message">
+        <div className={`link-bar ${showMenu ? 'show-menu' : ''}`}>
+        <Link to="/Message">
             <BiSolidMessageDetail size={40} className="react-icons" />
           </Link>
           <span>
-            <Link to="/Message">Message</Link>
+          <Link to="/Message">Message</Link>
           </span>
         </div>
-        
-        <div className="link-bar">
-          <Link to="/Notifications">
+        <div className={`link-bar ${showMenu ? 'show-menu' : ''}`}>
+        <Link to="/Notifications">
             <IoIosNotifications size={40} className="react-icons" />
           </Link>
           <span>
-            <Link to="/Notifications">Notify</Link>
+          <Link to="/Notifications">Notify</Link>
           </span>
         </div>
-        
-        <div className="link-bar">
-          <Link to={`/Profile/${ownerId}`}>
+        <div className={`link-bar ${showMenu ? 'show-menu' : ''}`}>
+        <Link to={`/Profile/${ownerId}`}>
             <BiUserCircle size={40} className="react-icons" />
           </Link>
           <span>
             <Link to={`/Profile/${ownerId}`}>Profile</Link>
           </span>
-        
         </div>
       </div>
     </div>
